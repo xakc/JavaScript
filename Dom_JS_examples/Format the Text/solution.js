ce8a38c0326c1fabe0f897a8a5bfa5224d209153
xakc <n3dev@abv.bg>
2021-03-30 18:57:30 +0300

    Adding Format text example

--- Dom_JS_examples/Format the Text/solution.js
+++ Dom_JS_examples/Format the Text/solution.js
@@ -1,5 +1,19 @@
 function solve() {
-  
+    let input = document.getElementById('input');
+    let text = input.innerText;
 
+    let outputElement = document.getElementById('output');
+    let sentances = text.split('.');
+
+    while(sentances.length > 0) {
+        let currentParagraph = sentances.splice(0,3).join('.');
+
+        let paragraphElement = document.createElement('p');
+        paragraphElement.innerText = currentParagraph;
+        outputElement.appendChild(paragraphElement);
+    }
+    
 
 }
+
+  
