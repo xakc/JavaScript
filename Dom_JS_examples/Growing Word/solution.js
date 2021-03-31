64da6f6f5347fbb224bd9d40b00e88102259dac0
xakc <n3dev@abv.bg>
2021-03-31 09:45:37 +0300

    Adding some dom example projects:

--- Dom_JS_examples/Growing Word/solution.js
+++ Dom_JS_examples/Growing Word/solution.js
@@ -1,23 +1,39 @@
-function growingWord() {
+// function growingWord() {
 
-  let heading = document.querySelector('h1');
-  let currentColorElement = document.querySelector
+//   let heading = document.querySelector('h1');
+//   let currentColorElement = document.querySelector
 
-  if (!heading.style.fontSize){
-      let firstColourElement = document.getElementById('blueDiv');
-      firstColourElement.setAttribute('current-color',true);
+//   if (!heading.style.fontSize){
+//       let firstColourElement = document.getElementById('blueDiv');
+//       firstColourElement.setAttribute('current-color',true);
       
 
-     heading.style.fontSize '2px';
-     heading.style.color='blue';
-  } else {
-    let size = parseInt(heading.style.fontSize)*2;
-     heading.style.fontSize = `${size}px`;
+//      heading.style.fontSize '2px';
+//      heading.style.color='blue';
+//   } else {
+//     let size = parseInt(heading.style.fontSize)*2;
+//      heading.style.fontSize = `${size}px`;
 
-     let currentColorElement= document.querySelector('div[data-current=true]');
-     
+//      let colorsElelemnts= document.querySelector('#colors');
+    
 
-  } 
+//   } 
 
+
+// }
+
+
+  function solution() {
+
+    let parentElement = document.getElementById('exercise');
+    let growingWorldElement =parentElement.lastElementChild;
+    
+    
+      if(!growingWorldElement.style.fontSize) {
+          growingWorldElement.style.fontSize = '2px';
+      } else {
+        growingWorldElement.style.fontSize = (parseInt(growingWorldElement.style.fontSize)*2)+'px';
+
+      }
 
 }
--- Dom_JS_examples/Visited Websites/solution.js
+++ Dom_JS_examples/Visited Websites/solution.js
@@ -1,4 +1,19 @@
 function solve() {
+  // let linkElements = document.getElementsByClassName('link-1');
+  let linkElements = document.querySelectorAll('.link-1 a');
+  
+ 
+  for (const linkElement of linkElements) {
+    linkElement.addEventListener('click', onLinkElementClick)
+   
+  }
 
-  //TODO...
+  function  onLinkElementClick(e) {
+    let paragraphElement = e.currentTarget.nextElementSibling; 
+    let visitedCount = Number(paragraphElement.innerText.split(' ')[1]);
+    visitedCount++;
+
+    paragraphElement.innerText= `visited ${visitedCount} times`;
+  }
+
 }
