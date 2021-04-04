e9d9ee7abbffd6ae0f822486fa02025203ffcadf
xakc <n3dev@abv.bg>
2021-04-04 18:47:50 +0300

    Adding Furniture JS example PART 1

---
+++ Dom_JS_examples/Furniture/solution.js
@@ -0,0 +1,31 @@
+function solve() {
+  let [generate,buy] =  [...document.querySelectorAll('button')];
+
+  [...generate.querySelectorAll('button')][0]
+    .addEventListener('click',()=>{
+      let input = document.querySelectorAll('textarea')[0];
+      let products = JSON.parse(input.value); 
+
+      products.
+          forEach(product=>{
+              let {name, img,price,decFactor} = product;
+              let htmlString = `<tr>\n
+                                    <td>img src="${img}"></td>\n
+                                    <td><p>${name}</p></td>\n
+                                    <td>><p>${price}</p></td>\n
+                                    <td>><p>${decFactor}</p></td>\n
+                                    <td>><input type="checkbox" /></td>\n
+                                  </tr>
+                                `;
+              document.querySelectorAll('tbody')[0].insertAdjacentHTML('beforeEnd',htmlString);
+
+          })
+
+          input.value='';
+    });
+
+     buy.addEventListener('click',()=>{
+
+    });
+  
+}
